820945790654fe91d2f76def83ae07893473598d
Sulaeman <sulaiman.tsany@gmail.com>
2022-10-20 06:18:13 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // safelist: [
+  //   {
+  //     pattern: /.*/,
+  //     variants: [
+  //       "hover",
+  //       "focus",
+  //       "focus-visible",
+  //       "active",
+  //       "disabled",
+  //     ],
+  //   },
+  // ],
+};
